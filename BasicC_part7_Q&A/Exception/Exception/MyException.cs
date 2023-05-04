214b8cb2170174ff1b3f432f7ecd66b1bd6a2710
Teodora Emilia Berende <TeodoraEmilia.Berende@endava.com>
2023-05-04 15:56:05 +0300

    Example of MyException

---
+++ BasicC_part7_Q&A/Exception/Exception/MyException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    public class MyException : Exception
+    {
+        private string _details;
+        public MyException(string message, string details) : base(message)
+        {
+            _details = details;
+        }
+
+        public string GetDetails()
+        {
+            return _details;
+        }
+    }
+}
---
+++ BasicC_part7_Q&A/Exception/Exception/Program.cs
@@ -0,0 +1,17 @@
+﻿using Exceptions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        try
+        {
+            //something
+        }
+        catch (MyException ex) { }
+        catch (Exception ex) { }
+
+    }
+}
